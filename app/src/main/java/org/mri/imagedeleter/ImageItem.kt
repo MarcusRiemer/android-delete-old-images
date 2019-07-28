f0045be810566c5d5cbfc4c0c06da8c35a13b516
Marcus Riemer <Marcus@GurXite.de>
2019-07-28 11:04:17 +0200

    Now using EXIF date

--- app/src/main/java/org/mri/imagedeleter/ImageItem.kt
+++ app/src/main/java/org/mri/imagedeleter/ImageItem.kt
@@ -2,6 +2,9 @@ package org.mri.imagedeleter
 
 import android.content.Context
 import android.text.format.DateFormat
+import com.drew.imaging.ImageMetadataReader
+import com.drew.metadata.Metadata
+import com.drew.metadata.exif.ExifSubIFDDirectory
 import java.io.File
 import java.text.DecimalFormat
 import java.util.*
@@ -10,10 +13,12 @@ data class ImageItem(
     val path: String
 ) {
     val file = File(path)
-    private val creationDate = Date(file.lastModified())
+    private val metadata = ImageMetadataReader.readMetadata(file)
+        .getFirstDirectoryOfType(ExifSubIFDDirectory::class.java)
 
-    fun formattedDate(ctx: Context): String = DateFormat.getDateFormat(ctx).format(creationDate)
+    fun creationDate() = metadata.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL)
 
+    fun formattedDate(ctx: Context): String = DateFormat.getDateFormat(ctx).format(creationDate())
 
     private fun readableFileSize(size: Long): String {
         if (size <= 0) return "0"
--- app/src/main/java/org/mri/imagedeleter/MainActivity.kt
+++ app/src/main/java/org/mri/imagedeleter/MainActivity.kt
@@ -4,16 +4,21 @@ import android.Manifest
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.SeekBar
-import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import android.app.DatePickerDialog
+import android.text.format.DateFormat
+import android.widget.EditText
+import java.util.*
+import java.text.SimpleDateFormat
 
 
 class MainActivity : AppCompatActivity() {
 
 
     private val MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 12
+
+    private var editTextImagesSince: EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,10 +44,35 @@ class MainActivity : AppCompatActivity() {
             return;
         }
 
+
+        val sharedBeginCalendar = Calendar.getInstance()
+        editTextImagesSince = findViewById<EditText>(R.id.edit_text_date)
+        val date = DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+
+            sharedBeginCalendar.set(Calendar.YEAR, year)
+            sharedBeginCalendar.set(Calendar.MONTH, monthOfYear)
+            sharedBeginCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateLabel(sharedBeginCalendar)
+        }
+
+        editTextImagesSince!!.setOnClickListener {
+            DatePickerDialog(
+                this@MainActivity, date, sharedBeginCalendar
+                    .get(Calendar.YEAR), sharedBeginCalendar.get(Calendar.MONTH),
+                sharedBeginCalendar.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        }
+
         val imagesAdapter = ImagesAdapter(this)
 
         val recyclerView = findViewById<RecyclerView>(R.id.images_recycler_view)
         recyclerView.layoutManager = GridLayoutManager(this, 2)
         recyclerView.adapter = imagesAdapter
+    }
+
+    private fun updateLabel(c: Calendar) {
+        val format = DateFormat.getDateFormat(this)
+
+        editTextImagesSince!!.setText(format.format(c.getTime()))
     }
 }
